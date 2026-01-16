16e46d4abf30eeb0d8a4f7980202f409f3948f3e
Maria Makarova <mamakaro@f1br9s2.clusters.42paris.fr>
2026-01-16 11:22:50 +0100

    ex02 CPP02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+/*
+Pointeur = variable qui contnent une addresse de la variable sur laquelle
+elle pointe
+Reference: un alias d'une variable existante
+La reference ne peut pas etre nulle, a la meme cible (const), 
+*/
+int main(void)
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string* stringPTR = &string;
+    std::string& stringREF = string;
+
+    std::cout << "Memory address of the string: " << &string << std::endl;
+    std::cout << "Memory address of the stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address of the stringREF: " << &stringREF << std::endl;
+
+    std::cout << "\n";
+
+    std::cout << "The value of the string: " << string << std::endl;
+    std::cout << "The value of the stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value of the stringREF: " <<stringREF << std::endl;
+
+}
